f8efbc46334990d789af97e292956dd4c597f839
yashVSawant <yashsawant23910@gmail.com>
2024-10-20 13:32:03 +0530

    create user model

---
+++ backend/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
+  about: { type: String, required: true, default: "I am Chatter😁" },
+});
+
+module.exports = mongoose.model("User", userSchema);
